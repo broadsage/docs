ef37224772f1f35e65558f38d46ca6c1c7062954
Jeh1928 <jbanimineni@broadsage.com>
2025-07-22 22:09:26 +0530

    feat: added dependabot for pkg updates
    Signed-off-by: Jeh1928 <jbanimineni@broadsage.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui-pro'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro'],
   devtools: {
-    enabled: true
+    enabled: true,
   },
   css: ['~/assets/css/main.css'],
   routeRules: {
-    '/': { prerender: true }
+    '/': { prerender: true },
   },
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
   compatibilityDate: '2025-07-15',
   eslint: {
     config: {
       stylistic: {
         commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        braceStyle: '1tbs',
+      },
+    },
+  },
+});
